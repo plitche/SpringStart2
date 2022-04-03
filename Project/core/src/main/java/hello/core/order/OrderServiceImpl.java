e25e08253f7d062bde43303207f014c333497347
plitche <kysu728@naver.com>
2022-04-03 21:09:50 +0900

    15.스프링 핵심 원리 이해2 - 객체 지향 원리 적용 - 관심사의 분리

--- Project/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Project/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,19 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     // -> DIP, OCP 위반됨 구현이 아닌 interface에만 의존하게 해야함.
 
     // final은 무조건 값이 할당 되어야 함으로 지운다. -> 근데 이러면 NPE가 발생함.
     // -> 해결하려면 누군가 클라이언트인 OrderServiceImpl에 DiscountPlicy의 객체를 대신 생성하고 주입 해주어야 한다.
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
