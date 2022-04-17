dd069307e1d5c68e405af630d9b064a95225dd65
plitche <kysu728@naver.com>
2022-04-17 22:11:54 +0900

    35. 싱글톤 컨테이너 - @Configuration과 바이트코드 조작의 마법

--- Project/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ Project/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -2,16 +2,14 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ConfigurationSingletoneTest {
+public class ConfigurationSingletonTest {
 
     @Test
     void configurationTest() {
@@ -29,6 +27,14 @@ public class ConfigurationSingletoneTest {
 
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
     }
 
 }
