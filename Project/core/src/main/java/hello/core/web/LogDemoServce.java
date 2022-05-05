727bb99abc4ac53a390eaeee4f8ea680e22bf225
plitche <kysu728@naver.com>
2022-05-06 00:22:30 +0900

    57. 빈 스코프 - request 스코프 예제 마들기

--- Project/core/src/main/java/hello/core/web/LogDemoServce.java
+++ Project/core/src/main/java/hello/core/web/LogDemoServce.java
@@ -1,2 +1,16 @@
-package hello.core.web;public class LogDemoServce {
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoServce {
+
+    private final MyLogger myLogger;
+
+    public void logic(String testId) {
+        myLogger.log("Service.Id = " + testId);
+    }
 }
